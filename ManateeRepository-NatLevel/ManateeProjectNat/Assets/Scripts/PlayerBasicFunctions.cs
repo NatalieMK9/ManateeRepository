72bb532e94dbf823fbbff60b89654c7631463821
NatalieMK9 <64727532+NatalieMK9@users.noreply.github.com>
2021-03-30 09:17:20 -0400

    added functions

---
+++ ManateeRepository-NatLevel/ManateeProjectNat/Assets/Scripts/PlayerBasicFunctions.cs
@@ -0,0 +1,35 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBasicFunctions : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        public int playerHealth = 2;
+    }
+
+public class ItemBehavior : MonoBehaviour
+{
+    // 1
+    void OnCollisionEnter(Collision collision)
+    {
+        // 2
+        if (collision.gameObject.name == "single boat")
+        {
+            // 3
+            Destroy(this.transform.parent.gameObject);
+            // 4
+            Debug.Log("You lost health");
+        }
+    }
+
+
+// Update is called once per frame
+void Update()
+    {
+        
+    }
+}*/
