797a7dd8dac5b52ad48c2b9527e52485d48a38dc
fairyfade <77501073+fairyfade@users.noreply.github.com>
2021-03-30 07:58:48 -0400

    Final

---
+++ Assets/Scripts/moveCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveCamera : MonoBehaviour
+{
+    //camera speed
+    public float speed = 1.0f;
+    public GameObject Manatee;
+
+    //positions
+    private Transform target;
+       void moveForward()
+    {
+        //move Camera
+        Camera.main.transform.position = new Vector3(1.18f, 5.76f, -30.45f);
+    }
+}
